38602ce10265b470b08d23920695c72a3f63c5b7
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-02-22 00:10:38 -0500

    Changed the main page

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@
 import Image from "next/image";
 import { redirect } from 'next/navigation'
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  redirect('/docs/Machine_Learning_Proposal.pdf');
-  // return (
-  //   <main className={styles.main}>
-        
-  //   </main>
-  // );
+  return (
+    <main className={styles.main}>
+      <Link href="/docs/Machine_Learning_Proposal.pdf" locale={false}>Link to Proposal</Link>
+    </main>
+  );
 }
