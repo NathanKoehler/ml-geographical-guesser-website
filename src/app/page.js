cd16e73104adb3c7b56b85a7c12154edfc041d34
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-02-22 00:26:56 -0500

    Changing the rendering of the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-"use client";
-
-import Image from "next/image";
-import { redirect } from 'next/navigation'
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Link href="/docs/Machine_Learning_Proposal.pdf" locale={false}>Link to Proposal</Link>
+      <Link href="/docs/Machine_Learning_Proposal.pdf">Link to Proposal</Link>
     </main>
   );
 }
