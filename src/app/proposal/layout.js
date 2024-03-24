f6dc2492cd099359aa3aa69595bdde0fe98f8327
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-03-23 20:38:30 -0400

    Changed theme

--- src/app/proposal/layout.js
+++ src/app/proposal/layout.js
@@ -1,18 +1,15 @@
 "use client";
 
 import { Box, Container, CssVarsProvider } from "@mui/joy";
-import Header from "@/app/proposal/components/Header";
-import styles from "@/app/page.module.css";
+import Header from "./components/Header";
 
 export default function ProposalLayout({ children }) {
   return (
     <CssVarsProvider defaultMode="dark">
-      <main className={styles.main}>
         <Box sx={{ position: "absolute", top: 0, left: 0, width: "100dvw", height: "100dvh" }}>
           <Header />
           { children }
         </Box>
-      </main>
     </CssVarsProvider>
   );
 }
