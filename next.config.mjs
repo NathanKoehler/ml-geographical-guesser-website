fd53899d15fad4005bfd42c233a8204e06c32766
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-03-26 18:04:35 -0400

    Changed the deployment process

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/ml-geographical-guesser-website',
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
